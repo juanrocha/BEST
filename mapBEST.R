9e5db82a86485df3c46d74936237652b5ce71fc9
juanrocha <aguilajk@gmail.com>
2016-04-12 17:58:22 +0200

    fixed zoom and save data

--- mapBEST.R
+++ mapBEST.R
@@ -7,8 +7,17 @@
 require(ggmap)
 
 # get map from Google
-map <- get_map('Cienaga Grande de Santa Marta', zoom=9, maptype = 'terrain', color='bw', source='osm')
+map <- get_map('Cienaga Grande de Santa Marta', zoom=10, maptype = 'roadmap', color='bw', source='google')
 class(map)
+
+# use a box
+bbox <- attr(map, 'bb')
+bbox <- bb2bbox(bbox)
+# Modify manually bbox to get the range you want, check googlemaps for the right coords.
+bbox2 <- c(-74.83926, 10.59559, -74.07983, 11.51344) # left, bottom, right, top 
+
+# query google again with your coords
+map <- get_map(bbox2, zoom=10, maptype = 'toner', color='bw', source='stamen')
 
 # get point coordinates from Google
 fieldSites <- c('Taganga', 'Tasajeras, Magdalena', 'Buenavista, Sitionuevo', 'Las Flores, Barranquilla')
@@ -19,4 +28,12 @@ PopSize <- c(3000, # 3000 ppl Taganga from wikipedia
 
 coords <- geocode(fieldSites)
 
-ggmap(map) + geom_point(aes(x=lon, y=lat, colour='red', size=2), data=coords, alpha=0.5)
+g <- ggmap(map) + geom_point(aes(x=lon, y=lat, colour='red', size=2), data=coords, alpha=0.5)
+
+## Get Colombia also for context
+
+Colombia <- get_map('Colombia', zoom=5, maptype = 'roadmap', color='bw', source='google')
+c <- ggmap(Colombia)
+
+
+# save.image(file='mapBEST.RData', safe=T)
